e6977d3ef9cbedbb688292403394b260ead22f0b
crowhound <jonathan.westfall1@gmail.com>
2024-11-06 12:46:39 -0600

    Added a new constructor to SFRow and marked the old USSClassName as obsolete.
    Added:
    Added a SFRow class constructor that allows for taking in a array of strings for the parameter.

--- Runtime/Layouts/SFRow.cs
+++ Runtime/Layouts/SFRow.cs
@@ -1,3 +1,5 @@
+using System;
+
 using UnityEngine.UIElements;
 
 namespace SF.UIElements
@@ -5,7 +7,9 @@ namespace SF.UIElements
     [UxmlElement]
     public partial class SFRow : VisualElement
     {
-        public const string USSClassName = "row";
+        [Obsolete("We are now using sf-row as the class to to prevent any conflicts with clas names users write them selves. This is just kept to prevent breaking projects already using it.")]
+        public const string ObsoleteUSSClassName = "row";
+        public const string USSClassName = "sf-row";
 
         public SFRow()
         {
@@ -16,6 +20,18 @@ namespace SF.UIElements
         {
             if(!string.IsNullOrEmpty(className))
                 AddToClassList(className);
+        }
+
+        public SFRow(string[] classNames) : this()
+        {
+            if(classNames.Length < 1)
+                return;
+
+            for(int i = 0; i < classNames.Length; i++)
+            {
+                if(!string.IsNullOrEmpty(classNames[i]))
+                    AddToClassList(classNames[i]);
+            }
         }
     }
 }
