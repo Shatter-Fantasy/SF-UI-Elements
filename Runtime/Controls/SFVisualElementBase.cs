96a006058f645f75030d8256813186364612a0f7
8BitsPerByte <jonathan.westfall1@gmail.com>
2025-09-13 16:14:33 -0500

    Added a clone visual tree asset method for the  SFVisualElementBase class.

--- Runtime/Controls/SFVisualElementBase.cs
+++ Runtime/Controls/SFVisualElementBase.cs
@@ -4,14 +4,29 @@ using UnityEngine.UIElements;
 
 namespace SF.UIElements
 {
-    [UxmlElement]
+  
     public partial class SFVisualElementBase : VisualElement
     {
         public const string USSClassName = "sf-element";
+        /// <summary>
+        /// This is the main visual tree asset for the SFVisualElement.
+        /// This is mainly used in child classes.
+        /// </summary>
+        protected VisualTreeAsset _visualTreeAsset;
+        protected VisualElement _rootTemplateContainer = new();
         
         public SFVisualElementBase()
         {
             this.AddClass(USSClassName);
+        }
+
+        protected virtual void CloneVisualTreeAsset()
+        {
+            if (_visualTreeAsset == null)
+                return;
+            
+            _visualTreeAsset.CloneTree(_rootTemplateContainer);
+            Add(_rootTemplateContainer);
         }
     }
 }
